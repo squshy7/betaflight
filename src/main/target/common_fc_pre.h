f7958d99400b291a0e3862d631e761c0add0fb4d
UAV Tech <spatzengr@yahoo.com>
2019-02-04 13:34:16 -0500

    updoing Performance Edition removes from FC_pre.h

--- src/main/target/common_fc_pre.h
+++ src/main/target/common_fc_pre.h
@@ -125,15 +125,15 @@
 #define USE_CLI
 #define USE_GYRO_REGISTER_DUMP  // Adds gyroregisters command to cli to dump configured register values
 #define USE_PPM
-//#define USE_PWM
+#define USE_PWM
 #define USE_SERIAL_RX
 #define USE_SERIALRX_CRSF       // Team Black Sheep Crossfire protocol
 #define USE_SERIALRX_IBUS       // FlySky and Turnigy receivers
 #define USE_SERIALRX_SBUS       // Frsky and Futaba receivers
 #define USE_SERIALRX_SPEKTRUM   // SRXL, DSM2 and DSMX protocol
-//#define USE_SERIALRX_SUMD       // Graupner Hott protocol
-//#define USE_SERIALRX_SUMH       // Graupner legacy protocol
-//#define USE_SERIALRX_XBUS       // JR
+#define USE_SERIALRX_SUMD       // Graupner Hott protocol
+#define USE_SERIALRX_SUMH       // Graupner legacy protocol
+#define USE_SERIALRX_XBUS       // JR
 
 
 
@@ -144,21 +144,21 @@
 #endif
 
 #if (FLASH_SIZE > 64)
-//#define USE_ACRO_TRAINER
+#define USE_ACRO_TRAINER
 #define USE_BLACKBOX
 #define USE_LED_STRIP
 #define USE_RESOURCE_MGMT
 #define USE_RUNAWAY_TAKEOFF     // Runaway Takeoff Prevention (anti-taz)
-//#define USE_SERVOS
+#define USE_SERVOS
 #define USE_TELEMETRY
 #define USE_TELEMETRY_FRSKY_HUB
-//#define USE_TELEMETRY_HOTT
-//#define USE_TELEMETRY_LTM
+#define USE_TELEMETRY_HOTT
+#define USE_TELEMETRY_LTM
 #define USE_TELEMETRY_SMARTPORT
 #define USE_ITERM_RELAX
 #define USE_RC_SMOOTHING_FILTER
-//#define USE_ABSOLUTE_CONTROL
-//#define USE_SMART_FEEDFORWARD
+#define USE_ABSOLUTE_CONTROL
+#define USE_SMART_FEEDFORWARD
 #define USE_THROTTLE_BOOST
 #endif
 
@@ -169,11 +169,11 @@
 #define USE_DSHOT_DMAR
 #define USE_GYRO_OVERFLOW_CHECK
 #define USE_YAW_SPIN_RECOVERY
-//#define USE_HUFFMAN
+#define USE_HUFFMAN
 #define USE_MSP_DISPLAYPORT
 #define USE_MSP_OVER_TELEMETRY
-//#define USE_PINIO
-//#define USE_PINIOBOX
+#define USE_PINIO
+#define USE_PINIOBOX
 #define USE_RCDEVICE
 #define USE_RTC_TIME
 #define USE_RX_MSP
@@ -191,10 +191,10 @@
 #define USE_ESC_SENSOR_INFO
 #define USE_CRSF_CMS_TELEMETRY
 #define USE_BOARD_INFO
-//#define USE_SMART_FEEDFORWARD
-//#define USE_THROTTLE_BOOST
-//#define USE_RC_SMOOTHING_FILTER
-//#define USE_ITERM_RELAX
+#define USE_SMART_FEEDFORWARD
+#define USE_THROTTLE_BOOST
+#define USE_RC_SMOOTHING_FILTER
+#define USE_ITERM_RELAX
 
 #ifdef USE_SERIALRX_SPEKTRUM
 #define USE_SPEKTRUM_BIND
@@ -225,5 +225,5 @@
 #define USE_TELEMETRY_MAVLINK
 #define USE_UNCOMMON_MIXERS
 #define USE_SIGNATURE
-//#define USE_ABSOLUTE_CONTROL
+#define USE_ABSOLUTE_CONTROL
 #endif
