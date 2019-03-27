4948dfb39e8f1ad771ef67d9fa7268be5eae2883
Bruce Luckcuck <github@etracer.net>
2019-03-27 06:45:15 -0400

    Feedforward return factor Adds a `feedforward_return_factor` parameter that controls the scaling of the feedforward contribution when the stick is returning to center. Default is 100 (100%) so the contribution is the same regardless of stick direction. Setting to 0 will limit feedforward to only affecting when the stick is moving away from center (initiation of the move). Any other value scales the feedforward contribution when stick is returning to center (ending the move).

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -127,7 +127,7 @@ static FAST_RAM_ZERO_INIT float airmodeThrottleOffsetLimit;
 
 #define LAUNCH_CONTROL_YAW_ITERM_LIMIT 50 // yaw iterm windup limit when launch mode is "FULL" (all axes)
 
-PG_REGISTER_ARRAY_WITH_RESET_FN(pidProfile_t, PID_PROFILE_COUNT, pidProfiles, PG_PID_PROFILE, 9);
+PG_REGISTER_ARRAY_WITH_RESET_FN(pidProfile_t, PID_PROFILE_COUNT, pidProfiles, PG_PID_PROFILE, 10);
 
 void resetPidProfile(pidProfile_t *pidProfile)
 {
@@ -201,6 +201,7 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .motor_output_limit = 100,
         .auto_profile_cell_count = AUTO_PROFILE_CELL_COUNT_STAY,
         .transient_throttle_limit = 15,
+        .feedforward_return_factor = 100,
     );
 #ifdef USE_DYN_LPF
     pidProfile->dterm_lowpass_hz = 150;
@@ -560,6 +561,8 @@ static FAST_RAM_ZERO_INIT float dMinGyroGain;
 static FAST_RAM_ZERO_INIT float dMinSetpointGain;
 #endif
 
+static FAST_RAM_ZERO_INIT float feedForwardReturnFactor;
+
 void pidInitConfig(const pidProfile_t *pidProfile)
 {
     if (pidProfile->feedForwardTransition == 0) {
@@ -703,6 +706,8 @@ void pidInitConfig(const pidProfile_t *pidProfile)
 #if defined(USE_AIRMODE_LPF)
     airmodeThrottleOffsetLimit = pidProfile->transient_throttle_limit / 100.0f;
 #endif
+
+    feedForwardReturnFactor = pidProfile->feedforward_return_factor / 100.0f;
 }
 
 void pidInit(const pidProfile_t *pidProfile)
@@ -1420,9 +1425,13 @@ void FAST_CODE pidController(const pidProfile_t *pidProfile, timeUs_t currentTim
         // Only enable feedforward for rate mode and if launch control is inactive
         const float feedforwardGain = (flightModeFlags || launchControlActive) ? 0.0f : pidCoefficient[axis].Kf;
         if (feedforwardGain > 0) {
+            // If feedforward is helping (pushing the same direction as setpoint) then the
+            // stick is moving away from center to start teh move - use 100% feedforward.
+            // Otherwise stick is returning to center so scle feedforward effect.
+            const float stickDirectionFactor = (currentPidSetpoint * pidSetpointDelta > 0) ? 1.0f : feedForwardReturnFactor;
             // no transition if feedForwardTransition == 0
             float transition = feedForwardTransition > 0 ? MIN(1.f, getRcDeflectionAbs(axis) * feedForwardTransition) : 1;
-            pidData[axis].F = feedforwardGain * transition * pidSetpointDelta * pidFrequency;
+            pidData[axis].F = feedforwardGain * transition * stickDirectionFactor * pidSetpointDelta * pidFrequency;
 
 #if defined(USE_SMART_FEEDFORWARD)
             applySmartFeedforward(axis);
